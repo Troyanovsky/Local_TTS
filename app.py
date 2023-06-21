a6692e871f83b1fd6b7f6e7aae7b49f32bff7d8b
troyanovsky <524619251@qq.com>
2023-06-21 17:48:38 +0800

    Added TTS with Voice Conversion

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-# pip install TTS; Python version < 3.11
+# Run the commands in terminal first to install module and models; Python version < 3.11
+# pip install TTS
+# tts --model_name "tts_models/en/ljspeech/fast_pitch"
+# tts --model_name "vocoder_models/en/ljspeech/hifigan_v2"
+# tts --model_name "voice_conversion_models/multilingual/vctk/freevc24"
 import tkinter as tk
 from tkinter import filedialog
 from TTS.api import TTS
 from tkinter import messagebox
 import os
+import datetime
 
 class TTSApp:
     def __init__(self, master):
@@ -28,6 +33,14 @@ class TTSApp:
         self.output_folder_label = tk.Label(master, text="")
         self.output_folder_label.pack()
 
+        # Add "Clone Source" button
+        self.clone_source_button = tk.Button(master, text="Clone Source", command=self.choose_source_file)
+        self.clone_source_button.pack()
+
+        # Add label to display source file name
+        self.source_file_label = tk.Label(master, text="")
+        self.source_file_label.pack()
+
         # Initialize TTS object
         self.model_name = "tts_models/en/ljspeech/fast_pitch"
         self.vocoder_name = "vocoder_models/en/ljspeech/hifigan_v2"
@@ -39,19 +52,29 @@ class TTSApp:
 
         # Get output folder path from label
         output_folder_path = self.output_folder_label.cget("text")
+        clone_source_path = self.source_file_label.cget("text")
 
         # Set output folder path to default if user does not choose a folder
         if not output_folder_path:
             output_folder_path = self.default_output_folder
 
-        # Generate audio file from user input
-        self.tts.tts_to_file(text=text, file_path=output_folder_path + "/output.wav")
+        # Generate a unique output file name with a timestamp
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        output_file_name = f"output_{timestamp}.wav"
+        output_file_path = os.path.join(output_folder_path, output_file_name)
 
-        # Generate audio file from user input
-        self.tts.tts_to_file(text=text, file_path=output_folder_path + "/output.wav")
-
-        # Show message box when generation is done
-        messagebox.showinfo("Generation Complete", "Audio file generated successfully!")
+        try:
+            if clone_source_path:
+                # Generate audio file from user input and reference wav file for voice cloning
+                self.tts.tts_with_vc_to_file(text=text, file_path=output_file_path, speaker_wav=clone_source_path)
+            else:
+                # Generate audio file from user input
+                self.tts.tts_to_file(text=text, file_path=output_file_path)
+            # Show message box when generation is done
+            messagebox.showinfo("Generation Complete", "Audio file generated successfully!")
+        except Exception as e:
+            # Show message box when generation is in error
+            messagebox.showinfo("Generation Error", f"Audio file cannot be generated. Error: {str(e)}")
 
     def choose_output_folder(self):
         # Open file dialog to choose output folder
@@ -63,6 +86,17 @@ class TTSApp:
 
         # Update output folder label with selected folder path
         self.output_folder_label.config(text=output_folder_path)
+    
+    def choose_source_file(self):
+        # Open file dialog to choose source file
+        source_file_path = filedialog.askopenfilename(filetypes=[("WAV files", "*.wav")])
+
+        # Set source file path to empty string if user does not choose a file
+        if not source_file_path:
+            source_file_path = ""
+
+        # Update source file label with selected file path
+        self.source_file_label.config(text=source_file_path)
 
 root = tk.Tk()
 app = TTSApp(root)
